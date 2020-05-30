454beebef6192a30d5021e4ee3882f37908f4128
CodePurble <CodePurble@users.noreply.github.com>
2020-05-30 07:46:30 +0000

    deploy: 09bb652a551d50a663e947069f4d9561cb80949f

--- all_12.js
+++ all_12.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['welcome_20to_20documentation_20for_20_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2fcodepurble_2frp_2dscrabble_22_3erp_2dscrabble_3c_2fa_3e_21_153',['Welcome to documentation for &lt;a href=&quot;https://github.com/CodePurble/rp-scrabble&quot;&gt;rp-scrabble&lt;/a&gt;!',['../index.html',1,'']]],
+  ['welcome_20to_20documentation_20for_20rp_2dscrabble_21_153',['Welcome to documentation for rp-scrabble!',['../index.html',1,'']]],
   ['wordsinplay_154',['wordsInPlay',['../classPlay.html#acdea15fed5ef5976c8f29fc379f27f2a',1,'Play']]]
 ];
--- search/pages_0.js
+++ search/pages_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['welcome_20to_20documentation_20for_20_3ca_20href_3d_22https_3a_2f_2fgithub_2ecom_2fcodepurble_2frp_2dscrabble_22_3erp_2dscrabble_3c_2fa_3e_21_339',['Welcome to documentation for &lt;a href=&quot;https://github.com/CodePurble/rp-scrabble&quot;&gt;rp-scrabble&lt;/a&gt;!',['../index.html',1,'']]]
+  ['welcome_20to_20documentation_20for_20rp_2dscrabble_21_339',['Welcome to documentation for rp-scrabble!',['../index.html',1,'']]]
 ];
