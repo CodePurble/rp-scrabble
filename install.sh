6fc1f84cf8339ec63557e72c1928d4b08ebb8345
Ramprakash Chandrashekar <ramprakash.chandrashekar@gmail.com>
2020-12-03 14:35:07 +0530

    change build system to meson

--- install.sh
+++ install.sh
@@ -1,69 +1,69 @@
 #!/bin/bash
 
-BUILD_DIR=build/
+BUILD_DIR=build
 EXEC=rp-scrabble
 INSTALL_DIR=$HOME/.local/bin
 LOG_DIR=$HOME/.local/share/rp-scrabble/logs
-LOG_PARENT_DIR=$HOME/.local/share/rp-scrabble/
+LOG_PARENT_DIR=$HOME/.local/share/rp-scrabble
 
 
 case $1 in
-    "debian")
-        if ! cmake --version > /dev/null 2>&1; then
-            echo "cmake not installed, it is required for building from source"
-            echo "Installing cmake..."
-            if ! sudo apt update; then
-                echo "Aborting install"
-                exit 1
-            fi
-            if ! sudo apt install cmake; then
-                exit 1
-            fi
-        else
-            echo "cmake is installed"
-        fi
-        ;;
+	"debian")
+		if ! meson --version > /dev/null 2>&1; then
+			echo "meson not installed, it is required for building from source"
+			echo "Installing meson..."
+			if ! sudo apt update; then
+				echo "Aborting install"
+				exit 1
+			fi
+			if ! sudo apt install meson; then
+				exit 1
+			fi
+		else
+			echo "meson is installed"
+		fi
+		;;
 
-    "arch")
-        if ! cmake --version > /dev/null 2>&1; then
-            echo "cmake not installed, it is required for building from source"
-            echo "Installing cmake..."
-            if ! sudo pacman -S cmake; then
-                echo "Aborting install"
-                exit 1
-            fi
-        else
-            echo "cmake is installed"
-        fi
-        ;;
+	"arch")
+		if ! meson --version > /dev/null 2>&1; then
+			echo "meson not installed, it is required for building from source"
+			echo "Installing meson..."
+			if ! sudo pacman -S meson; then
+				echo "Aborting install"
+				exit 1
+			fi
+		else
+			echo "meson is installed"
+		fi
+		;;
 
-    "help")
-        echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
-        exit 0
-        ;;
+	"help")
+		echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
+		exit 0
+		;;
 
-    "uninstall")
-        echo "Uninstalling rp-scrabble..."
-        rm -f --verbose $INSTALL_DIR/$EXEC
-        rm -rdf --verbose $LOG_PARENT_DIR
-        exit 0
-        ;;
+	"uninstall")
+		echo "Uninstalling rp-scrabble..."
+		rm -f --verbose $INSTALL_DIR/$EXEC
+		rm -rdf --verbose $LOG_PARENT_DIR
+		exit 0
+		;;
 
-    "custom")
-        if ! cmake --version > /dev/null 2>&1; then
-            echo "cmake not installed, it is required for building from source"
-            echo "Please install it using your distribution's package manager and rerun this script"
-            exit 1
-        else
-            echo "cmake is installed"
-        fi
-        ;;
+	"custom")
+		if ! meson --version > /dev/null 2>&1; then
+			echo "meson not installed, it is required for building from source"
+			echo "Please install it using your distribution's package manager and rerun this script"
+			exit 1
+		else
+			echo "meson is installed"
+		fi
+		;;
 
-    *)
-        echo "Invalid arguments"
-        echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
-        exit 1
-        ;;
+	*)
+		echo "Invalid arguments"
+		echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
+		exit 1
+		;;
 esac
 
 
@@ -72,11 +72,9 @@ if [ ! -d "$BUILD_DIR" ]; then mkdir -vp $BUILD_DIR; fi
 if [ ! -d "$INSTALL_DIR" ]; then mkdir -vp $INSTALL_DIR; fi
 if [ ! -d "$LOG_DIR" ]; then mkdir -vp $LOG_DIR; fi
 
-cd $BUILD_DIR
-
 echo "Building from source.."
-cmake -DCMAKE_BUILD_TYPE=Release .. && make
+meson --buildtype=release $BUILD_DIR && ninja -C $BUILD_DIR
 
 echo "Installing..."
-cp --verbose $EXEC $INSTALL_DIR
+cp --verbose $BUILD_DIR/$EXEC $INSTALL_DIR
 chmod 755 $INSTALL_DIR/$EXEC
