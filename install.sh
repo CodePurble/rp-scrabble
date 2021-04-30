ea2dd5875668c5d0adc3faf3a835c7091b0362ff
Ramprakash Chandrashekar <49452779+CodePurble@users.noreply.github.com>
2021-04-30 23:17:13 +0530

    tabs to spaces (why do I do this?)

--- install.sh
+++ install.sh
@@ -8,62 +8,62 @@ LOG_PARENT_DIR=$HOME/.local/share/rp-scrabble
 
 
 case $1 in
-	"debian")
-		if ! meson --version > /dev/null 2>&1; then
-			echo "meson not installed, it is required for building from source"
-			echo "Installing meson..."
-			if ! sudo apt update; then
-				echo "Aborting install"
-				exit 1
-			fi
-			if ! sudo apt install meson; then
-				exit 1
-			fi
-		else
-			echo "meson is installed"
-		fi
-		;;
+    "debian")
+        if ! meson --version > /dev/null 2>&1; then
+            echo "meson not installed, it is required for building from source"
+            echo "Installing meson..."
+            if ! sudo apt update; then
+                echo "Aborting install"
+                exit 1
+            fi
+            if ! sudo apt install meson; then
+                exit 1
+            fi
+        else
+            echo "meson is installed"
+        fi
+        ;;
 
-	"arch")
-		if ! meson --version > /dev/null 2>&1; then
-			echo "meson not installed, it is required for building from source"
-			echo "Installing meson..."
-			if ! sudo pacman -S meson; then
-				echo "Aborting install"
-				exit 1
-			fi
-		else
-			echo "meson is installed"
-		fi
-		;;
+    "arch")
+        if ! meson --version > /dev/null 2>&1; then
+            echo "meson not installed, it is required for building from source"
+            echo "Installing meson..."
+            if ! sudo pacman -S meson; then
+                echo "Aborting install"
+                exit 1
+            fi
+        else
+            echo "meson is installed"
+        fi
+        ;;
 
-	"help")
-		echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
-		exit 0
-		;;
+    "help")
+        echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
+        exit 0
+        ;;
 
-	"uninstall")
-		echo "Uninstalling rp-scrabble..."
-		rm -f --verbose $INSTALL_DIR/$EXEC
-		rm -rdf --verbose $LOG_PARENT_DIR
-		exit 0
-		;;
+    "uninstall")
+        echo "Uninstalling rp-scrabble..."
+        rm -f --verbose $INSTALL_DIR/$EXEC
+        rm -rdf --verbose $LOG_PARENT_DIR
+        exit 0
+        ;;
 
-	"custom")
-		if ! meson --version > /dev/null 2>&1; then
-			echo "meson not installed, it is required for building from source"
-			echo "Please install it using your distribution's package manager and rerun this script"
-			exit 1
-		else
-			echo "meson is installed"
-		fi
-		;;
+    "custom")
+        if ! meson --version > /dev/null 2>&1; then
+            echo "meson not installed, it is required for building from source"
+            echo "Please install it using your distribution's package manager and rerun this script"
+            exit 1
+        else
+            echo "meson is installed"
+        fi
+        ;;
 
-	*)
-		echo "Invalid arguments"
-		echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
-		exit 1
-		;;
+    *)
+        echo "Invalid arguments"
+        echo "Usage: ./install.sh [ arch | debian | help | uninstall | custom ]"
+        exit 1
+        ;;
 esac
 
 
--- src/bag.cc
+++ src/bag.cc
@@ -18,163 +18,163 @@ enum_location loc = BAG;
  */
 Bag::Bag()
 {
-	Tile* curTile = nullptr;
-	/* Add a's */
-	for(int i = 0; i < 9; i++) {
-		curTile = new Tile('A', 1, loc);
-		bag.push_back(curTile);
-	}
+    Tile* curTile = nullptr;
+    /* Add a's */
+    for(int i = 0; i < 9; i++) {
+        curTile = new Tile('A', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add b's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('B', 3, loc);
-		bag.push_back(curTile);
-	}
+    /* Add b's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('B', 3, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add c's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('C', 3, loc);
-		bag.push_back(curTile);
-	}
+    /* Add c's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('C', 3, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add d's */
-	for(int i = 0; i < 4; i++) {
-		curTile = new Tile('D', 2, loc);
-		bag.push_back(curTile);
-	}
+    /* Add d's */
+    for(int i = 0; i < 4; i++) {
+        curTile = new Tile('D', 2, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add e's */
-	for(int i = 0; i < 12; i++) {
-		curTile = new Tile('E', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add e's */
+    for(int i = 0; i < 12; i++) {
+        curTile = new Tile('E', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add f's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('F', 4, loc);
-		bag.push_back(curTile);
-	}
+    /* Add f's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('F', 4, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add g's */
-	for(int i = 0; i < 3; i++) {
-		curTile = new Tile('G', 2, loc);
-		bag.push_back(curTile);
-	}
+    /* Add g's */
+    for(int i = 0; i < 3; i++) {
+        curTile = new Tile('G', 2, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add h's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('H', 4, loc);
-		bag.push_back(curTile);
-	}
+    /* Add h's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('H', 4, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add i's */
-	for(int i = 0; i < 9; i++) {
-		curTile = new Tile('I', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add i's */
+    for(int i = 0; i < 9; i++) {
+        curTile = new Tile('I', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add j */
-	curTile = new Tile('J', 8, loc);
-	bag.push_back(curTile);
+    /* Add j */
+    curTile = new Tile('J', 8, loc);
+    bag.push_back(curTile);
 
-	/* Add k */
-	curTile = new Tile('K', 5, loc);
-	bag.push_back(curTile);
+    /* Add k */
+    curTile = new Tile('K', 5, loc);
+    bag.push_back(curTile);
 
-	/* Add l's */
-	for(int i = 0; i < 4; i++) {
-		curTile = new Tile('L', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add l's */
+    for(int i = 0; i < 4; i++) {
+        curTile = new Tile('L', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add m's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('M', 3, loc);
-		bag.push_back(curTile);
-	}
+    /* Add m's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('M', 3, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add n's */
-	for(int i = 0; i < 6; i++) {
-		curTile = new Tile('N', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add n's */
+    for(int i = 0; i < 6; i++) {
+        curTile = new Tile('N', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add o's */
-	for(int i = 0; i < 8; i++) {
-		curTile = new Tile('O', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add o's */
+    for(int i = 0; i < 8; i++) {
+        curTile = new Tile('O', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add p's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('P', 3, loc);
-		bag.push_back(curTile);
-	}
+    /* Add p's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('P', 3, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add q */
-	curTile = new Tile('Q', 10, loc);
-	bag.push_back(curTile);
+    /* Add q */
+    curTile = new Tile('Q', 10, loc);
+    bag.push_back(curTile);
 
-	/* Add r's */
-	for(int i = 0; i < 6; i++) {
-		curTile = new Tile('R', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add r's */
+    for(int i = 0; i < 6; i++) {
+        curTile = new Tile('R', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add s's */
-	for(int i = 0; i < 4; i++) {
-		curTile = new Tile('S', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add s's */
+    for(int i = 0; i < 4; i++) {
+        curTile = new Tile('S', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add t's */
-	for(int i = 0; i < 6; i++) {
-		curTile = new Tile('T', 1, loc);
-		bag.push_back(curTile);
-	}
+    /* Add t's */
+    for(int i = 0; i < 6; i++) {
+        curTile = new Tile('T', 1, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add u's */
-	for(int i = 0; i < 4; i++) {
-		curTile = new Tile('U', 3, loc);
-		bag.push_back(curTile);
-	}
+    /* Add u's */
+    for(int i = 0; i < 4; i++) {
+        curTile = new Tile('U', 3, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add v's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('V', 4, loc);
-		bag.push_back(curTile);
-	}
+    /* Add v's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('V', 4, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add w's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('W', 4, loc);
-		bag.push_back(curTile);
-	}
-	/* Add x */
-	curTile = new Tile('X', 8, loc);
-	bag.push_back(curTile);
+    /* Add w's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('W', 4, loc);
+        bag.push_back(curTile);
+    }
+    /* Add x */
+    curTile = new Tile('X', 8, loc);
+    bag.push_back(curTile);
 
-	/* Add y's */
-	for(int i = 0; i < 2; i++) {
-		curTile = new Tile('Y', 4, loc);
-		bag.push_back(curTile);
-	}
+    /* Add y's */
+    for(int i = 0; i < 2; i++) {
+        curTile = new Tile('Y', 4, loc);
+        bag.push_back(curTile);
+    }
 
-	/* Add z */
-	curTile = new Tile('Z', 10, loc);
-	bag.push_back(curTile);
+    /* Add z */
+    curTile = new Tile('Z', 10, loc);
+    bag.push_back(curTile);
 
-	/*
-	 * [> Add blanks <]
-	 * for(int i = 0; i < 2; i++){
-	 *     curTile = new Tile('#', 0, loc);
-	 *     bag.push_back(curTile);
-	 * }
-	 */
+    /*
+     * [> Add blanks <]
+     * for(int i = 0; i < 2; i++){
+     *     curTile = new Tile('#', 0, loc);
+     *     bag.push_back(curTile);
+     * }
+     */
 
-	for(auto& t : bag) {
-		t->setBag(this);
-	}
+    for(auto& t : bag) {
+        t->setBag(this);
+    }
 }
 
 /**
@@ -182,11 +182,11 @@ Bag::Bag()
  */
 Bag::~Bag()
 {
-	for(Tile* t : bag) {
-		delete t;
-	}
+    for(Tile* t : bag) {
+        delete t;
+    }
 
-	bag.clear();
+    bag.clear();
 }
 
 /**
@@ -194,10 +194,10 @@ Bag::~Bag()
  */
 void Bag::show()
 {
-	for(Tile* i : bag) {
-		cout << i->getLetter();
-	}
-	BOLD(" (" + to_string(bag.size()) + " tiles remaining)\n");
+    for(Tile* i : bag) {
+        cout << i->getLetter();
+    }
+    BOLD(" (" + to_string(bag.size()) + " tiles remaining)\n");
 }
 
 /**
@@ -207,16 +207,16 @@ void Bag::show()
  */
 void Bag::shuffle()
 {
-	int j;
-	Tile* temp = nullptr;
-	srand(time(NULL));
+    int j;
+    Tile* temp = nullptr;
+    srand(time(NULL));
 
-	for(unsigned long i = 0; i < bag.size(); i++) {
-		j = rand() % bag.size(); // Generate random index
-		temp = bag[i];
-		bag[i] = bag[j];
-		bag[j] = temp;
-	}
+    for(unsigned long i = 0; i < bag.size(); i++) {
+        j = rand() % bag.size(); // Generate random index
+        temp = bag[i];
+        bag[i] = bag[j];
+        bag[j] = temp;
+    }
 
 }
 
@@ -231,16 +231,16 @@ void Bag::shuffle()
  */
 vector<Tile*> Bag::draw(int count)
 {
-	vector<Tile*> drawn;
-	unsigned long prev_size = bag.size();
+    vector<Tile*> drawn;
+    unsigned long prev_size = bag.size();
 
-	shuffle();
+    shuffle();
 
-	for(unsigned long i = 0; i < (unsigned long) count && i < prev_size; i++) {
-		drawn.push_back(bag.back());
-		bag.pop_back();
-	}
-	return drawn;
+    for(unsigned long i = 0; i < (unsigned long) count && i < prev_size; i++) {
+        drawn.push_back(bag.back());
+        bag.pop_back();
+    }
+    return drawn;
 }
 
 /**
@@ -252,5 +252,5 @@ vector<Tile*> Bag::draw(int count)
  */
 bool Bag::isEmpty()
 {
-	return(bag.empty());
+    return(bag.empty());
 }
